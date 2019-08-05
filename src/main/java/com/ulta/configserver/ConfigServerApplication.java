8566ef3cd947ff1acb675b7f6694bb4428d0b151
Praval Kumar <Praval.Kumar@hcl.com>
2019-08-05 18:39:31 +0530

    minor changes

--- src/main/java/com/ulta/configserver/ConfigServerApplication.java
+++ src/main/java/com/ulta/configserver/ConfigServerApplication.java
@@ -3,7 +3,12 @@ package com.ulta.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+/**
+ * Config Server - 
+ * 		manages configuration on remote repo
+ * @author Praval.Kumar
+ *
+ */
 
 @EnableConfigServer
 @SpringBootApplication
